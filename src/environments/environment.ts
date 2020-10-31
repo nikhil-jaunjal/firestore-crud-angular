c70d934c860d15587806e3f7e3612b561de42732
Nikhil Jaunjal <31154769+nikhil-jaunjal@users.noreply.github.com>
2020-10-31 21:25:11 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBYs6zhh6CkiAidD8fWP447lPjxL_3_p54",
+    authDomain: "learningsaturdayfirestoredemo.firebaseapp.com",
+    databaseURL: "https://learningsaturdayfirestoredemo.firebaseio.com",
+    projectId: "learningsaturdayfirestoredemo",
+    storageBucket: "learningsaturdayfirestoredemo.appspot.com",
+    messagingSenderId: "852514656318",
+    appId: "1:852514656318:web:175b29624a7de20e43416f",
+    measurementId: "G-XMT4QG985G"
+  }
+};
+
